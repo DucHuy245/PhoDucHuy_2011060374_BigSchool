3fa9159eeb698da2b6b7b994252b922a904cfa7b
DucHuy245 <duchuypho245@gmail.com>
2023-03-19 00:48:39 +0700

    tuần 2

--- Controllers/CoursesController.cs
+++ Controllers/CoursesController.cs
@@ -1,4 +1,5 @@
-﻿using PhoDucHuy_2011060374_Bai3.Models;
+﻿using Microsoft.AspNet.Identity;
+using PhoDucHuy_2011060374_Bai3.Models;
 using PhoDucHuy_2011060374_Bai3.ViewModels;
 using System;
 using System.Collections.Generic;
@@ -15,6 +16,8 @@ namespace PhoDucHuy_2011060374_Bai3.Controllers
         {
             _dbContext = new ApplicationDbContext();
         }
+
+        [Authorize]
         // GET: Courses
         public ActionResult Create()
         {
@@ -23,6 +26,28 @@ namespace PhoDucHuy_2011060374_Bai3.Controllers
                 Categories = _dbContext.Categories.ToList()
             };
             return View(viewModel);
+        }
+
+        [Authorize]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(CourseViewModel viewModel)
+        {
+            if(!ModelState.IsValid)
+            {
+                viewModel.Categories = _dbContext.Categories.ToList();
+                return View("Create", viewModel);
+            }
+            var course = new Course
+            {
+                LecturerId = User.Identity.GetUserId(),
+                DateTime = viewModel.GetDateTime(),
+                CatogoryId = viewModel.Category,
+                Place = viewModel.Place,
+            };
+            _dbContext.Courses.Add(course);
+            _dbContext.SaveChanges();
+            return RedirectToAction("Index", "Home");
         }
     }
 }
---
+++ Migrations/202303170801122_AddNameColumnToApplicationUser1.cs
@@ -0,0 +1,18 @@
+﻿namespace PhoDucHuy_2011060374_Bai3.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class AddNameColumnToApplicationUser1 : DbMigration
+    {
+        public override void Up()
+        {
+            AddColumn("dbo.AspNetUsers", "Name", c => c.String(nullable: false, maxLength: 255));
+        }
+        
+        public override void Down()
+        {
+            DropColumn("dbo.AspNetUsers", "Name");
+        }
+    }
+}
